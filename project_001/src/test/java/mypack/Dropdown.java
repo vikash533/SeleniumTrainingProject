2fa9f783c669e089c0122ea76d4b4d76c9440e68
vikash533 <singhvikash533@gmail.com>
2023-07-06 16:23:19 +0530

    writing fist project

--- project_001/src/test/java/mypack/Dropdown.java
+++ project_001/src/test/java/mypack/Dropdown.java
@@ -22,13 +22,42 @@ public class Dropdown {
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
     }
-    @Test
+    @Test(priority = 1)
     void testScript1(){
         WebElement drp= driver.findElement(By.cssSelector("#ctl00_mainContent_DropDownListCurrency"));
         Select s = new Select(drp);
         s.selectByVisibleText("AED");
+        String text = s.getFirstSelectedOption().getText();
+        System.out.println("text of selected string:  "+text);
+
     }
-    @AfterTest
+    @Test(priority = 2)
+    void testScript2(){
+        WebElement drp1=driver.findElement(By.cssSelector("#divpaxinfo"));
+        drp1.click();
+        System.out.println(drp1.getText());
+        int i=1;
+        while(i<4){
+            driver.findElement(By.cssSelector("#hrefIncAdt")).click();
+            i++;
+        }
+        System.out.println(driver.findElement(By.cssSelector("#divpaxinfo")).getText());
+    }
+    @Test(priority = 3)
+    void testScript3(){
+       WebElement DeptCity = driver.findElement(By.cssSelector("span#ctl00_mainContent_ddl_originStation1_CTXTaction"));
+       DeptCity.click();
+       driver.findElement(By.xpath("//ul//li/a[.=' Ahmedabad (AMD)']")).click();
+       System.out.println(driver.findElement(By.xpath("//ul//li/a[.=' Ahmedabad (AMD)']")).getText());
+    }
+    @Test(priority = 4)
+    void testScript4(){
+    WebElement arrivalCity = driver.findElement(By.xpath("(//a[.=' Bengaluru (BLR)'])[2]"));
+    arrivalCity.click();
+    System.out.println(arrivalCity.getText());
+    driver.findElement(By.xpath("(//a[.='10'])[1]")).click();
+    }
+    @AfterTest(enabled = false)
     void tearDown(){
         driver.close();
     }
